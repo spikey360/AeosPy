ba6c67d266d7c676200f5f402fbc737a92ed623f
Compucrusade <spikey360@yahoo.co.in>
2012-06-22 12:50:53 +0530

    Written Client stub

--- msg.py
+++ msg.py
@@ -1,5 +1,13 @@
 #Message.py
 
+##DEVICE#STATES##
+init=0x01 #initialized
+conni=0x10 #connecting
+connd=0x100 #connected
+ready=0x1000 #ready
+busy=0x100000 #busy
+dead=0x1000000 #closed
+#################
 class Message:
 	numbers=[]
 	message=""
@@ -26,13 +34,36 @@ class Device:
 	devaddr=None
 	name=""
 	sock=None
+	state=0
 	
 	def __init__(self):
+		global init
 		print "Initializing"
+		state=init
 	
 	def getName(self):
 		return self.Name
 	
-	def establishConnection(self):
-		print "Establishing connection"
-	 
+	def acceptConnection(self):
+		global conni, connd
+		state=conni
+		print "Accepting connection"
+		state=connd
+	
+	def getClient(self):
+		print "returning instance of Client"
+	
+	def write(bytes):
+		print "Write bytes to output"
+	
+	def read(bytes):
+		print "Read bytes from input"
+
+class Client:
+	dev=None
+	def __init__(self,dev):
+		print "Client initialized"
+	def sendPicture(self,imgfile):
+		print "Send imgfile to app":
+	def getResponse(self):
+		print "return Response by reading from ip"
