d0292d33c9b48d8382b1bb448e2b588da1d7da33
Compucrusade <spikey360@yahoo.co.in>
2012-07-16 14:31:25 +0530

    writing OperationStore

--- btrmserver.py
+++ btrmserver.py
@@ -84,10 +84,12 @@ class Server:
 
 class RemoteDock:
 	server=None
+	oplist=None
+	controllable=None
 	def __init__(self,server):
 		self.server=server
 	
-	def sendOperationList(self,protocol,oplist):
+	def sendOperationList(self,oplist,protocol="l"):
 		msg=""
 		w=0
 		#oplist is a 3-tuple (image,title,opcode)
@@ -97,7 +99,7 @@ class RemoteDock:
 		#self.server.writeToRemote(protocol)
 		msg=msg+protocol
 		for k in oplist:
-			(title,opcode)=k
+			(title,opcode)=(oplist[k],k)
 			msg=msg+";"+title+":"+opcode
 		#self.server.writeToRemote("\0")
 		msg=msg+";\0"
@@ -107,10 +109,60 @@ class RemoteDock:
 		msg="p;"+name+";"+size+"\0"
 		self.server.writeToRemote(msg)
 	
-	def waitForOpcode():
+	def readProtocolString():
+		###########
+		ps=self.server.readFromRemote()
+		p=ps[:1] #the type of query
+		if p=="d":
+			op=int(ps[2:])
+			(name,size)=self.oplist.getDetails(op)
+			self
+			#write a protocol string
+			return
+		if p=="f":
+			op=int(ps[2:])
+			dat=self.oplist.getThumb(op)
+			#write thumbnail to remote
+			return
+		if p=="x":
+			op=int(ps[2:])
+			self.controllable.opControl(op)
+			#write an acknowledgement
+			return
+		if p=="q":
+			#quit, release resources and wait for the next connection
+			return
+		#d for details reply with p;<name>;<size in B>
+		#f for fetching picture, to implement later
+		#x for executing opcode coming with it
+		
+		###########
 		self.server.writeToRemote("[w]\0") #[w] tells remote that server waiting for response
 		msg=self.server.readFromRemote()
 		#expected format "[r]<opcode>"
 		return msg
-		
 
+class OperatorStore:
+	oplist={}
+	detlist={}
+	thumblist={}
+	
+	def __init__(self,startdir):
+		#self.generateDetails(startdir)
+	
+	def getOplist(self):
+		return self.oplist
+	
+	def getDetail(self,opcode):
+		return self.detlist[opcode]
+	
+	def addOperation(self, operation):
+		#operation is a tuple of format (<operation>,<opcode>)
+		(opname,opcode)=operation
+		self.oplist[opcode]=opname
+		
+	def addDetails(self,details):
+		#details is a tuple of format (<details>,<opcode>)
+		#<details> is a dictionary
+		(det,opcode)=details
+		self.detlist[opcode]=det
