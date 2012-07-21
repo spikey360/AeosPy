9e784ebac44590a43b4d7c8fa9523260d440038c
Compucrusade <spikey360@yahoo.co.in>
2012-07-21 16:06:01 +0530

    self.presentZoomLevel bug fixed

--- view.py
+++ view.py
@@ -3,8 +3,9 @@ import os
 
 class PicturePanel(wx.Panel):
 	bmp=None
+	presentZoomLevel=1.00
 	def __init__(self,parent):
-		wx.Panel.__init__(self,parent,size=(600,300))
+		wx.Panel.__init__(self,parent,size=(890,500))
 		self.frame=parent
 		self.Bind(wx.EVT_PAINT,self.onPaint)
 		
@@ -13,25 +14,70 @@ class PicturePanel(wx.Panel):
 		Add a picture to the background
 		"""
 		# yanked from ColourDB.py
-		
-		img=wx.Image(self.bmp)
-		iw=img.GetWidth()
-		ih=img.GetHeight()
-		ar=(iw+0.0)/(ih+0.0)
-		nh=300
-		nw=300*ar
-		bmp=img.Scale(nw,nh).ConvertToBitmap()
-		self.SetSize((bmp.GetWidth(),bmp.GetHeight()))
 		dc = None
 		if not dc:
 			dc = wx.ClientDC(self)
 			rect = self.GetUpdateRegion().GetBox()
 			dc.SetClippingRect(rect)
-		dc.Clear()
+		##now draw image
+		img=wx.Image(self.bmp)
+		iw=img.GetWidth()
+		ih=img.GetHeight()
+		nw=iw
+		nh=ih
+		ar=(iw+0.0)/(ih+0.0)
+		w,h=dc.GetSize()
+		if iw>=w:
+			nw=w
+			nh=w/ar
+		if nh>h:
+			nh=h
+			#nw=h/ar
 		
-		dc.DrawBitmap(bmp, 0, 0)
+		bmp=img.Scale(nw,nh).ConvertToBitmap()
+		self.SetSize((bmp.GetWidth(),bmp.GetHeight()))
+		presentZoomLevel=1
+		dc.Clear()
+		dc.DrawBitmap(bmp, w/2-nw/2, h/2-nh/2)
+		
 	def setImage(self,bmp):
 		self.bmp=bmp
+		
+	def showZoomedImage(self,zoom=True):
+		#decrease zoom level
+		if zoom==True:
+			if self.presentZoomLevel>0.2:
+				self.presentZoomLevel-=0.1
+		else:
+			if self.presentZoomLevel<1:
+				self.presentZoomLevel+=0.1
+		dc = None
+		if not dc:
+			dc = wx.ClientDC(self)
+			rect = self.GetUpdateRegion().GetBox()
+			dc.SetClippingRect(rect)
+		##now draw image
+		img=wx.Image(self.bmp)
+		iw=img.GetWidth()
+		ih=img.GetHeight()
+		nw=iw
+		nh=ih
+		ar=(iw+0.0)/(ih+0.0)
+		w,h=dc.GetSize()
+		if iw>=w:
+			nw=w
+			nh=w/ar
+		if nh>h:
+			nh=h
+			#nw=h/ar
+		sw=iw*self.presentZoomLevel
+		sh=ih*self.presentZoomLevel
+		
+		bmp=img.GetSubImage((0,0,sw,sh)).Scale(nw,nh).ConvertToBitmap()
+		self.SetSize((bmp.GetWidth(),bmp.GetHeight()))
+		
+		dc.Clear()
+		dc.DrawBitmap(bmp, w/2-nw/2, h/2-nh/2)
 
 
 class Viewer(wx.Frame):
@@ -39,9 +85,12 @@ class Viewer(wx.Frame):
 	dash=None
 	nextButton=None
 	prevButton=None
+	fsButton=None
 	piclist=None
 	albumSize=0
 	curr=0
+	isFs=False
+	conlist={}
 	def __init__(self,title,winsize,folder):
 		
 		wx.Frame.__init__(self,None,title=title+" "+folder,size=winsize)
@@ -55,18 +104,30 @@ class Viewer(wx.Frame):
 		nextButton.Bind(wx.EVT_BUTTON,self.onNext)
 		prevButton=wx.Button(dpanel,label="<")
 		prevButton.Bind(wx.EVT_BUTTON,self.onPrev)
+		fsButton=wx.Button(dpanel,label="Fullscreen")
+		fsButton.Bind(wx.EVT_BUTTON,self.onFullScreen)
+		zoomButton=wx.Button(dpanel,label="+")
+		zoomButton.Bind(wx.EVT_BUTTON,self.onZoomed)
+		moozButton=wx.Button(dpanel,label="-")
+		moozButton.Bind(wx.EVT_BUTTON,self.onMoozed)
 		dpanel.SetSizer(dash)
 		dpanel.Layout()
 		dash.Add(prevButton,0,wx.LEFT)
 		dash.Add(nextButton,0,wx.RIGHT)
+		dash.Add(fsButton,0,wx.BOTTOM)
+		dash.Add(zoomButton,0,wx.TOP)
+		dash.Add(moozButton,0,wx.TOP)
 		mainBox=wx.BoxSizer(wx.VERTICAL)
 		mainBox.Add(self.picPanel,0,wx.EXPAND)
 		mainBox.Add(dpanel,0,wx.BOTTOM)
 		panel.SetSizer(mainBox)
 		panel.Layout()
-		
+		panel.SetAutoLayout(True)
 		self.picPanel.setImage(self.piclist[self.curr])
-		
+		#add all controls to conlist
+		self.conlist[1]=("Fullscreen",self.toggleFullScreen)
+		self.conlist[2]=("Zoom",self.zoom)
+		self.conlist[3]=("Mooz",self.mooz)
 		
 	def onClose(self,event):
 		self.Destroy()
@@ -82,6 +143,42 @@ class Viewer(wx.Frame):
 		
 		self.picPanel.setImage(self.piclist[self.curr])
 		self.picPanel.Refresh()
+	
+	def onFullScreen(self,event):
+		self.toggleFullScreen()
+	
+	def onZoomed(self,event):
+		self.zoom()
+	
+	def onMoozed(self,event):
+		self.mooz()
+	########################################################
+	def toggleFullScreen(self):
+		if self.isFs==False:
+			self.ShowFullScreen(True,style=wx.FULLSCREEN_ALL)
+			self.isFs=True
+		else:
+			self.ShowFullScreen(False)
+			self.isFs=False
+		self.picPanel.Refresh()
+	
+	def zoom(self):
+		self.picPanel.showZoomedImage()
+	
+	def mooz(self):
+		self.picPanel.showZoomedImage(zoom=False)
+	#########################################################
+	def opControl(self,op):
+		#op an integer
+		self.curr=op-1
+		#change self.curr accordingly
+		self.picPanel.setImage(self.piclist[self.curr])
+		self.picPanel.Refresh()
+		
+	def conControl(self,op):
+		(n,f)=self.conlist[op]
+		f()
+		
 	def setAlbumFolder(self,x):
 		if os.access(x,os.R_OK)==False:
 			return
@@ -95,9 +192,9 @@ class Viewer(wx.Frame):
 				self.piclist[self.albumSize]=x+y
 				self.albumSize=self.albumSize+1
 
-app=wx.App(redirect=False)
+#app=wx.App(redirect=False)
 
-view=Viewer(title="AeosPy Viewer",winsize=(500,400),folder="/home/riju/Pictures/wallpaper/")
-view.Show()
+#view=Viewer(title="AeosPy Viewer",winsize=(500,400),folder="/home/riju/Pictures/wallpaper/")
+#view.Show()
 
-app.MainLoop()
+#app.MainLoop()
